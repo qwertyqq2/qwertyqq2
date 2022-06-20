89c7885d37177841db5399ef722fc29f47d78504
aksuker <48645092+qwertyqq2@users.noreply.github.com>
2022-06-20 19:07:33 +0300

    Create USERS.sql

---
+++ DB/USERS.sql
@@ -0,0 +1,12 @@
+CREATE TABLE USERS (
+    Id int NOT NULL AUTO_INCREMENT,
+    Email varchar(255),
+    PRIMARY KEY (Id)
+); 
+
+ INSERT INTO USERS VALUES (1, "ivanov@mail.ru");
+ INSERT INTO USERS VALUES (2, "SemahcokOlga@mail.ru");
+ INSERT INTO USERS VALUES (3, "Mmmurochka@rambler.ru");
+ INSERT INTO USERS VALUES (4, "artemIcov@mail.ru");
+ INSERT INTO USERS VALUES (5, "axenovVova@student.ru");
+ INSERT INTO USERS VALUES (6, "alexanderSava@mail.ru");
